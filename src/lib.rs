cfc0ce81d3333caba0ebdc041468ae28fdad4b7d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-10 02:50:06 +0100

    stream skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,44 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate futures;
+
+use futures::{Async, Poll, Stream};
+
+/// A merkle tree stream.
+pub struct MerkleTree {
+  pub blocks: u64,
+}
+
+impl MerkleTree {
+  /// Create a new Merkle tree stream.
+  ///
+  /// ```
+  /// let stream = merkle_tree_stream::MerkleTree::new();
+  /// assert_eq!(stream.blocks, 0);
+  /// ```
+  pub fn new() -> MerkleTree {
+    MerkleTree { blocks: 0 }
+  }
+}
+
+impl Iterator for MerkleTree {
+  type Item = ();
+
+  fn next(&mut self) -> Option<()> {
+    None
+  }
+}
+
+impl Stream for MerkleTree {
+  type Item = ();
+  type Error = ();
+
+  /// Create a new Merkle tree stream.
+  ///
+  /// ```
+  /// let mut stream = merkle_tree_stream::MerkleTree::new();
+  /// let item = &stream.next();
+  /// assert!(item.is_none());
+  /// ```
+  fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+    Ok(Async::NotReady)
+  }
 }
