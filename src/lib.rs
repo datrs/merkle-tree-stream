6c98ace24735f04fd5f77c1030041c4790ccb322
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-20 10:41:27 +0100

    accept closures

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate futures;
 
-use futures::{Async, Poll};
+use futures::{Async, Poll, Stream};
+use std::error::Error;
 
 /// The data returned from the Stream.
 pub struct Chunk {
@@ -11,38 +12,20 @@ pub struct Chunk {
 }
 
 /// A merkle tree stream.
-pub struct Stream {
+pub struct MerkleTreeStream<L, P> {
   /// The amount of blocks stored.
   pub blocks: u64,
+  /// The root nodes.
   pub roots: Vec<i32>,
+  /// Closure that's called on each leaf node.
+  pub leaf_handler: L,
+  /// Closure that's called on each parent node.
+  pub parent_handler: P,
 }
 
-impl Stream {
-  /// Create a new Merkle tree stream.
-  ///
-  /// ```
-  /// let stream = merkle_tree_stream::Stream::new();
-  /// assert_eq!(stream.blocks, 0);
-  /// ```
-  pub fn new() -> Stream {
-    Stream {
-      blocks: 0,
-      roots: Vec::with_capacity(100),
-    }
-  }
-}
-
-impl Iterator for Stream {
-  type Item = ();
-
-  fn next(&mut self) -> Option<()> {
-    None
-  }
-}
-
-impl futures::Stream for Stream {
-  type Item = ();
-  type Error = ();
+impl<L, P> Stream for MerkleTreeStream<L, P> {
+  type Item = Chunk;
+  type Error = Box<Error>;
 
   fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
     Ok(Async::NotReady)
