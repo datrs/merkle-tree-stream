aa9ccac128565dc0b04c5fbdfab91504fd466ecf
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-24 00:53:23 +0100

    derives, docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,41 @@
+#![deny(warnings, missing_docs)]
+#![cfg_attr(test, feature(plugin))]
+#![cfg_attr(test, plugin(clippy))]
+
+//! A stream that generates a merkle tree based on incoming data. Adapted from
+//! [`mafintosh/merkle-tree-stream`](https://github.com/mafintosh/merkle-tree-stream).
+//!
+//! ## Why?
+//! Signatures & integrity checks are part of what makes Dat a great protocol. Each chunk that
+//! passes through the system is hashed and made part of a tree of hashes. We end up creating
+//! hashes of hashes thanks to [`flat-tree`](https://docs.rs/flat-tree), which in the end allows us
+//! to validate our complete data set.
+//!
+//! This module is only needed to create new Dat archives, but not to read them.
+//!
+//! ## See Also
+//! - [`flat-tree`](https://docs.rs/flat-tree)
+
 extern crate futures;
 
 use futures::{Async, Poll, Stream};
 use std::error::Error;
 
 /// The data returned from the Stream.
+#[derive(Debug, PartialEq, PartialOrd)]
 pub struct Chunk {
+  /// Offset from which the data was taken.
   pub index: u64,
+  /// Parent node.
   pub parent: u64,
+  /// Hash created from the hashing function
   pub hash: Vec<u8>,
+  /// Data that was hashed.
   pub data: Vec<u8>,
 }
 
 /// A merkle tree stream.
+#[derive(Debug, PartialEq, PartialOrd)]
 pub struct MerkleTreeStream<L, P> {
   /// The amount of blocks stored.
   pub blocks: u64,
@@ -22,6 +46,14 @@ pub struct MerkleTreeStream<L, P> {
   /// Closure that's called on each parent node.
   pub parent_handler: P,
 }
+
+// impl<L, P> MerkleTreeStream<L, P>
+//   where L: fn () -> i32,
+//         P: fn () -> i32,
+// {
+//   fn new (leaf_handler: L, parent_handler: P) -> i32 {
+//   }
+// }
 
 impl<L, P> Stream for MerkleTreeStream<L, P> {
   type Item = Chunk;
