89b3974ff68d881d9c1681bfa14be0a4248a46d7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-16 06:51:36 +0100

    split off merkle-tree-stream into own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// #![deny(warnings, missing_docs)]
+#![deny(warnings, missing_docs)]
 #![cfg_attr(test, feature(plugin))]
 #![cfg_attr(test, plugin(clippy))]
 
@@ -14,6 +14,49 @@
 //!
 //! This module is only needed to create new Dat archives, but not to read them.
 //!
+//! ## Usage
+//!
+//! ```rust
+//! extern crate merkle_tree_stream;
+//! extern crate rust_sodium;
+//!
+//! use merkle_tree_stream::{MerkleTreeStream, Node, PartialNode, StreamHandler};
+//! use rust_sodium::crypto::hash::sha256;
+//! use std::rc::Rc;
+//!
+//! struct S;
+//! impl StreamHandler for S {
+//!   fn leaf(&self, leaf: &PartialNode, _roots: &Vec<Rc<Node>>) -> Vec<u8> {
+//!     match leaf.data {
+//!       Some(ref data) => sha256::hash(&data).0.to_vec(),
+//!       None => panic!("Merkle tree stream did not have any data on the node. This should never happen."),
+//!     }
+//!   }
+//!
+//!   fn parent(&self, a: &Node, b: &Node) -> Vec<u8> {
+//!     let mut buffer: Vec<u8> = Vec::with_capacity(a.hash.len() + b.hash.len());
+//!     for c in &a.hash {
+//!       buffer.push(*c);
+//!     }
+//!     for c in &b.hash {
+//!       buffer.push(*c);
+//!     }
+//!     let digest = sha256::hash(&buffer);
+//!     digest.0.to_vec()
+//!   }
+//! }
+//!
+//! fn main() {
+//!   let roots = Vec::new();
+//!   let mut mts = MerkleTreeStream::new(S, roots);
+//!   let mut nodes: Vec<Rc<Node>> = Vec::new();
+//!   mts.next(b"hello", &mut nodes);
+//!   mts.next(b"hashed", &mut nodes);
+//!   mts.next(b"world", &mut nodes);
+//!   println!("nodes {:?}", nodes);
+//! }
+//! ```
+//!
 //! ## Installation
 //! ```sh
 //! $ cargo add merkle-tree-stream
@@ -22,108 +65,5 @@
 //! ## See Also
 //! - [`flat-tree`](https://docs.rs/flat-tree)
 
-extern crate flat_tree as flat;
-
-use std::rc::Rc;
-
-/// Node representation.
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-pub struct Node {
-  pub index: u64,
-  pub parent: u64,
-  pub size: usize,
-  pub hash: Vec<u8>,
-  pub data: Option<Vec<u8>>,
-}
-
-/// Intermediate Node representation. Same as Node, but without the `.hash` field.
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-pub struct PartialNode {
-  pub index: u64,
-  pub parent: u64,
-  pub size: usize,
-  pub data: Option<Vec<u8>>,
-}
-
-/// A vector of `Node` types.
-pub type NodeVector = Vec<Rc<Node>>;
-
-/// Functions that need to be implemented for `MerkleTreeStream`.
-pub trait StreamHandler {
-  fn leaf(&self, leaf: &PartialNode, roots: &NodeVector) -> Vec<u8>;
-  fn parent(&self, a: &Node, b: &Node) -> Vec<u8>;
-}
-
-/// Main constructor. Takes an instance of `MerkleTreeStream`.
-#[derive(Debug)]
-pub struct MerkleTreeStream<T> {
-  handler: T,
-  roots: NodeVector,
-  blocks: u64,
-}
-
-impl<T> MerkleTreeStream<T>
-where
-  T: StreamHandler,
-{
-  pub fn new(handler: T, roots: NodeVector) -> MerkleTreeStream<T> {
-    MerkleTreeStream {
-      handler: handler,
-      roots: roots,
-      blocks: 0,
-    }
-  }
-
-  /// Pass a string buffer through the flat-tree hash functions, and write the
-  /// result back out to "nodes".
-  pub fn next<'a>(&mut self, data: &[u8], nodes: &'a mut NodeVector) {
-    let index = 2 * self.blocks;
-    self.blocks = self.blocks + 1;
-
-    let leaf = PartialNode {
-      index: index,
-      parent: flat::parent(index),
-      size: 0,
-      data: Some(data.to_vec()),
-    };
-
-    let hash = self.handler.leaf(&leaf, &self.roots);
-    let leaf = Rc::new(Node {
-      index: leaf.index,
-      parent: leaf.parent,
-      size: leaf.size,
-      data: leaf.data,
-      hash: hash,
-    });
-
-    &self.roots.push(Rc::clone(&leaf));
-    &nodes.push(Rc::clone(&leaf));
-
-    while self.roots.len() > 1 {
-      let leaf = {
-        let left = &self.roots[self.roots.len() - 2];
-        let right = &self.roots[self.roots.len() - 1];
-
-        if left.parent != right.parent {
-          break;
-        }
-
-        Node {
-          index: left.parent,
-          parent: flat::parent(left.parent),
-          hash: self.handler.parent(&left, &right),
-          size: left.size + right.size,
-          data: None,
-        }
-      };
-
-      for _ in 0..2 {
-        &self.roots.pop();
-      }
-
-      let leaf = Rc::new(leaf);
-      &self.roots.push(Rc::clone(&leaf));
-      &nodes.push(Rc::clone(&leaf));
-    }
-  }
-}
+mod merkle_tree_stream;
+pub use merkle_tree_stream::*;
